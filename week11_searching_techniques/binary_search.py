cb5524452f43176325a61b2959a65e6dc9c9a201
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2024-11-26 13:55:44 +0000

    Update binary_search.py
    New version of binary search based on BBC bitesize code

--- week11_searching_techniques/binary_search.py
+++ week11_searching_techniques/binary_search.py
@@ -1,25 +1,66 @@
 '''
+Find <-- 7
+Found <-- False
+Start <-- 0
+End <-- length(list)
 
-1. Set low boundary to 0 and high boundary to l
-2. 
+WHILE Found = False AND Start <= End
+     Mid <-- (Start + End) DIV 2
+
+     IF list[Mid] = Find THEN
+        OUTPUT 'Found at' +  Mid
+        Found <-- True
+     ELSE
+        IF List[Mid] > Find THEN
+           End <-- Mid - 1
+        ELSE
+          Start <-- Mid + 1
+        ENDIF
+     ENDIF
+ENDWHILE
+
+IF Found = False THEN
+     OUTPUT 'Not found'
+ENDIF
 '''
 
-term = "kitchen"
+# Set up the data we look into
+sequence = [1,4,6,8,19,26,30,35,45,49,50,53,55,56,58,66,67,70,77,79,83,84,89,93,100]
+# When we start, we haven't found yet
+found = False
+# Starting point
+start = 0
+# Ending point
+end = len(sequence)
 
-f = open("words.txt")
-words = f.readlines()
+# Get the target number
+target = int(input("What number are you looking for?"))
 
-low_boundary = 0
-high_boundary = len(words) - 1
+# Repeat indefinitely
+# While number not found and
+# end >= start
+while found == False and start <= end:
 
-while low_boundary <= high_boundary:
-    middle = low_boundary + (high_boundary - low_boundary) // 2
+    # For visualisation: create a new list
+    search_space = sequence[start:end]
 
-    if words[middle].strip().lower() ==  term:
-        print("Found at", middle)
-    
-    elif words[middle] < term:
-        low_boundary = middle + 1
+    # Calculate midpoint index -> sum of start and end//2
+    midpoint = (start + end) // 2
+    mid_number = sequence[midpoint]
+
+    # If found - indicate so, break out of loop
+    if mid_number == target:
+        print("Found at index:", midpoint)
+        found = True
 
     else:
-        high_boundary = middle - 1
+        # If the midnumber > target
+        # Midpoint index is the new end of search space
+        # Otherwise index + is the new start of search space
+        if mid_number > target:
+            end = midpoint
+        else:
+            start = midpoint + 1
+
+if not found:
+    print("Not found")
